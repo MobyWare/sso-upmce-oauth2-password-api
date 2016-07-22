5c089c9fe0e172b02f54ed95ba4612a43e6c9314
dickm <dickm@upmc.edu>
2016-07-21 20:14:12 -0400

    fixed error linking controller to app

--- client/src/GreetingController.js
+++ client/src/GreetingController.js
@@ -1,15 +1,15 @@
-app.controller("GreetingApp", function($scope, $http){
-        $scope.getGreeting = function(){
-            $scope.greeting = "Click to get the greeting ...";
-            $http.get("http://localhost:8080/greeting")
-            .then(
-                function(response) {
-                    $scope.greeting = response.data;
-                },
-                function(){
-                    $scope.Greeting = 'Oops, something went wrong!!!';
-                }
-            );
-        }
+app.controller("GreetingController", function($scope, $http){
+    $scope.greeting = {content:"Click to get the greeting ..."};
+    $scope.getGreeting = function(){            
+        $http.get("http://localhost:8080/greeting")
+        .then(
+            function(response) {
+                $scope.greeting = response.data;
+            },
+            function(){
+                $scope.Greeting = 'Oops, something went wrong!!!';
+            }
+        );
     }
+}
 );
