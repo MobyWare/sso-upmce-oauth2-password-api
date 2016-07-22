7986d0d1ba5c65a0174abe0b708c9a33345c5eac
dickm <dickm@upmc.edu>
2016-07-22 10:42:38 -0400

    Got CORS working. added import.

--- spring-api/src/main/java/hello/GreetingController.java
+++ spring-api/src/main/java/hello/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class GreetingController {
@@ -11,6 +12,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin //along CORS
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
